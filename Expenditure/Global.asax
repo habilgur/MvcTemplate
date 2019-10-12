990e9aa21aefc9a53a237d135517964379fdea09
Asiaminor 78 <asiaminor78@hotmail.com>
2019-10-12 22:49:27 +0300

    Add project files.

---
+++ Expenditure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Expenditure.MvcApplication" Language="C#" %>
---
+++ Expenditure/Models/IdentityModels.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Expenditure.Models
+{
+
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+
+    //ÖNEMLİ NOT!!: Db First ile Asp Identity impementasyonu yapıldıysa Web Config Dosyasında iki 
+    // adet connection string dosyası olmalıdırç İlki EF Ado.Net ile Sql'den Tabloların çekilmesine yarayan string ikincisi ise Microsoft.AspNet.Identity'nin 
+    // haberleşecği connection string'dir.
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("ExpenditureIdentity", throwIfV1Schema:false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
